68928a4784a0a23a504639baef32ce0d9e95965c
Saranya-T3 <saranyatumati.345@gmail.com>
2025-12-09 18:43:40 +0530

    student.h

---
+++ student.h
@@ -0,0 +1,15 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <QString>
+
+class Student {
+public:
+    QString name;
+    QString subject;
+    int roll;
+    int semester;
+    QString grade;
+};
+
+#endif // STUDENT_H
